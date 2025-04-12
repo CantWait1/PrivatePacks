72cdbbbd3fec2ba81553e208ac926f74e3872463
CantWait <96596269+CantWait1@users.noreply.github.com>
2025-04-12 19:27:22 +0200

    Update security.ts

--- lib/security.ts
+++ lib/security.ts
@@ -7,7 +7,7 @@ export const passwordSchema = z
   .min(8, "Password must be at least 8 characters")
   .max(100, "Password must be less than 100 characters")
   .regex(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>\\'`~\[\]\/\-_=+])[A-Za-z\d!@#$%^&*(),.?":{}|<>\\'`~\[\]\/\-_=+]{8,}$/,
     "Password must include at least one uppercase letter, one lowercase letter, one number, and one special character"
   );
 
